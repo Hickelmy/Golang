32955c460ec9db5aaeb246615e212a11b12735c8
Riquelmy <riquelmy.silva@grupoicts.com.br>
2022-11-18 00:30:05 -0400

    first commit

---
+++ variaveis.go
@@ -0,0 +1,22 @@
+package main
+
+
+import "fmt"
+
+func main()  {
+	var variavel1 string = "Variavel 1"
+	variavel2 := "Variavel 2"
+
+	var(
+		variavel3 string = "Variavel 3" 
+		variavel4 string = "Variavel 4"
+	)
+
+
+
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+	fmt.Println(variavel3 , variavel4)
+	
+
+}
